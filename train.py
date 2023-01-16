2f17f838fe6cfb49a335d303ca3b4057feface5b
J. Pery <jperianey@alumnos.unex.es>
2023-01-16 17:08:36 +0100

    Added extra arguments and plotting training graph

--- train.py
+++ train.py
@@ -8,13 +8,16 @@ import pickle
 from inspect import getmembers, isfunction
 
 from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import callbacks
 from sklearn.utils import class_weight
 from config import SAMPLE_RATE, DEFAULT_WINDOW, DEFAULT_STEP, USE_MMAP, DEFAULT_BATCH_SIZE, DEFAULT_EPOCHS
 from model.model import AudioModelBuilder, DEFAULT_STFT_N_FFT, DEFAULT_STFT_WIN, DEFAULT_STFT_HOP, DEFAULT_N_MELS
 from loaders import FolderLoader, ClassLoaderFromFolderName
+from matplotlib import pyplot as plt
 
 AVAILABLE_KERAS_MODELS = {model_name: model for model_name, model in getmembers(importlib.import_module('tensorflow.keras.applications'), isfunction)}
+AVAILABLE_KERAS_OPTIMIZERS = {optimizer_name: optimizer for optimizer_name, optimizer in getmembers(importlib.import_module('tensorflow.keras.optimizers'), isfunction)}
 parser = argparse.ArgumentParser(prog = 'AudioTrain', description = 'Trains')
 parser.add_argument('folder')
 parser.add_argument('--model_id', default=int(time.time()))
@@ -29,6 +32,8 @@ parser.add_argument('--stft_win', default=DEFAULT_STFT_WIN, type=int, help='Each
 parser.add_argument('--stft_hop', default=DEFAULT_STFT_HOP, type=int, help='Number of samples between successive frames')
 parser.add_argument('--stft_nmels', default=DEFAULT_N_MELS, type=int, help='Number of Mel bands to generate')
 parser.add_argument('--model', default=None, choices=AVAILABLE_KERAS_MODELS.keys(), help='Any of the models of keras.applications that will be used as classification model')
+parser.add_argument('--optimizer', default=None, choices=AVAILABLE_KERAS_OPTIMIZERS.keys(), help='Any of the optimizers of keras.optimizers that will be used as optimizer in model training')
+parser.add_argument('--learning_rate', default=0.001, type=float)
 args = parser.parse_args()
 
 MODEL_ID = args.model_id
@@ -63,8 +68,10 @@ def train(x, y, num_classes):
     if args.model:
         predefined_model = AVAILABLE_KERAS_MODELS[args.model]
     model = AudioModelBuilder(**model_config).get_model(num_classes, predefined_model=predefined_model)
-    model.compile("adam", loss="categorical_crossentropy" if num_classes > 2 else "binary_crossentropy",
-                  metrics=['accuracy'])
+    optimizer = Adam
+    if args.optimizer:
+        optimizer = AVAILABLE_KERAS_OPTIMIZERS[args.optimizer]
+    model.compile(optimizer(learning_rate=args.learning_rate), loss="categorical_crossentropy" if num_classes > 2 else "binary_crossentropy", metrics=['accuracy'])
     checkpoint_filepath = 'checkpoints/%s/' % MODEL_ID
     model_checkpoint_callback = callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
@@ -76,10 +83,17 @@ def train(x, y, num_classes):
     reduce_lr = callbacks.ReduceLROnPlateau(verbose=1, min_delta=0.1, min_lr=1e-6)
     early_stopping = callbacks.EarlyStopping(monitor='val_loss', min_delta=0.01, patience=20, mode='auto', verbose=1)
     print("Starting training")
-    model.fit(x, y, validation_split=0.2, epochs=100, shuffle=True, batch_size=128,
+    history = model.fit(x, y, validation_split=0.2, epochs=args.epochs, shuffle=True, batch_size=args.batch_size,
               sample_weight=class_weight.compute_sample_weight('balanced', y),
               callbacks=[model_checkpoint_callback, reduce_lr, tboard, early_stopping])
-
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    plt.savefig(MODEL_ID + ".png")
 
 if __name__ == '__main__':
     train(*load_data())
