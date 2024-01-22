82eb6a5ec1edaae9eddbac09f7a049c63f33c931
J. Pery <jperianey@alumnos.unex.es>
2024-01-22 10:22:06 +0100

    Minor fix in import

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,3 +1,3 @@
 from .folder_loader import FolderLoader
 from .file_loader import FileLoader
-from .class_loader import ClassLoaderFromDict, ClassLoaderFromFolderName
+from .class_loader import ClassLoaderFromDict, ClassLoaderFromFolderName, ClassLoaderFromSameFileName
