73121ce8a6426e099d8025520af3453160bbafac
J. Pery <jperianey@alumnos.unex.es>
2024-01-22 10:16:23 +0100

    Updated constants

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ DEFAULT_WINDOW = 2 # Context (in seconds) for the audio processing
 DEFAULT_STEP = 1 # Every how many seconds the audio processing will be done
 DEFAULT_BATCH_SIZE = 128
 DEFAULT_EPOCHS = 100
+CHECKPOINTS_FOLDER = "checkpoints"
+MODEL_CONFIG_FOLDER = "model_config"
