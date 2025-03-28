eab434a609049588082bd2d28c3b7c14b27ee47b
J. Pery <jperianey@alumnos.unex.es>
2025-03-28 12:32:00 +0100

    Changed demo infer to fully support EnCodec format

--- demo_infer.py
+++ demo_infer.py
@@ -19,8 +19,8 @@ def infer(
         encodec,
         model_path,
         model_config_path=None,
+        loader_type='Audio',
         task='segment',
-        loader_type='Audio'
 ):
     if not model_config_path:
         base_path, model_name = model_path.rsplit('.', 1)[0].rsplit('/', 1)
@@ -50,7 +50,7 @@ with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
             audio = gr.Audio(label="Audio file to infer")
-            encodec = gr.Audio(label="Encodec file to infer", visible=False)
+            encodec = gr.File(label="Encodec file to infer", visible=False, file_types=[".ecdc"])
             inp = [
                 audio,
                 encodec,
@@ -65,40 +65,26 @@ with gr.Blocks() as demo:
                 )
             inp.append(data_type_dropdown)
 
-            def update_audio_visibility(type):  # Accept the event argument, even if not used
-                if type == 'Audio':
+            def update_visibility(selected_type, expected_type):  # Accept the event argument, even if not used
+                if selected_type == expected_type:
                     return gr.update(visible=True)
                 else:
                     return gr.update(visible=False)
 
-            data_type_dropdown.change(update_audio_visibility, data_type_dropdown, audio)
-
-
-            def update_encodec_visibility(type):  # Accept the event argument, even if not used
-                if type == 'Encodec':
-                    return gr.update(visible=True)
-                else:
-                    return gr.update(visible=False)
-            data_type_dropdown.change(update_encodec_visibility, data_type_dropdown, audio)
+            data_type_dropdown.change(lambda x: update_visibility(x, "Audio"), data_type_dropdown, audio)
+            data_type_dropdown.change(lambda x: update_visibility(x, "Encodec"), data_type_dropdown, encodec)
         with gr.Column():
             drop = gr.Dropdown(label="Prediction task", choices=list(TASK2MODEL.keys()))
-            out = [
-                gr.Label(label="Classification result"),
-                gr.Textbox(label="Segmentation result", visible=False)
-            ]
+            classification_result = gr.Label(label="Classification result")
+            segmentation_result = gr.Label(label="Segmentation result", visible=False)
+            out = [classification_result, segmentation_result]
     inp.append(drop)
     btn = gr.Button("Classify")
     btn.click(fn=infer, inputs=inp, outputs=out)
+    drop.change(lambda x: update_visibility(x, "classify"), drop, classification_result)
+    drop.change(lambda x: update_visibility(x, "segment"), drop, segmentation_result)
 
-    def update_visibility(prediction_type):  # Accept the event argument, even if not used
-        if prediction_type == 'classify':
-            return [gr.update(visible=True), gr.update(visible=False)]
-        else:
-            return [gr.update(visible=False), gr.update(visible=True)]
-
-    def update_task(prediction_type):
+    def update_task_name(prediction_type):
         return gr.update(value=prediction_type.capitalize())
-
-    drop.change(update_visibility, drop, out)
-    drop.change(update_task, drop, btn)
+    drop.change(update_task_name, drop, btn)
 demo.launch()
